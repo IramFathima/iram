a8a032cffbd685d91ef471d61471befdcffd7bd4
IramFathima <35484369+IramFathima@users.noreply.github.com>
2018-01-24 12:26:08 +0530

    Add files via upload

---
+++ programes.py
@@ -0,0 +1,6 @@
+#program to check number is positive or negative
+a=int(input("enter number to check positive or negative"))
+if a>0:
+    print("the given number is positive")
+else:
+    print("the given number is negative")
